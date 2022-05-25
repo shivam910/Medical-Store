9636daef642b4431ac6558d8b29c696955d3bcbe
shivam910 <shivams91099@gmail.com>
2022-05-25 22:56:54 +0530

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Screens
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 
+// Components
+import Navbar from "./Components/Navbar";
+
+
 function App() {
   return (
     <Router>
-        {/*Navbar*/}
+        <Navbar />
         {/*SideDrawer*/}
         {/*BackDrop*/}
         <main>
-          <Switch>
-            <Route exact path="/" component={HomeScreen} />
-            <Route exact path="/product/:id" component={ProductScreen} />
-            <Route exact path="/cart" component={CartScreen} />
-          </Switch>
+          <Routes>
+            <Route  path="/" element={<HomeScreen />} />
+            <Route  path="/product/:id" element={<ProductScreen />} />
+            <Route  path="/cart" element={<CartScreen />} />
+          </Routes>
         </main>
     </Router>
   );
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -12,13 +12,27 @@ const Navbar = () => {
       {/* Links */}
         <ul className="navbar__links">
             <li>
-                <Link to="/cart">
-                
+                <Link to="/cart" className="cart__link">
+                <i className="fas fa-shopping-cart"></i>
+                <span>
+                  Cart
+                  <span className="cartlogo__badge">0</span>
+                </span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/">
+                Shop  
                 </Link>
             </li>
         </ul>
 
       {/* Hamburger menu */}
+      <div className="hamburger__menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </nav>
   );
 };
